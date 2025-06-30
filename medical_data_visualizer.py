08b6409c5669e2754b1843799895a16df61af85f
Joshua Wright <1981283+SuperJoshua@users.noreply.github.com>
2025-06-30 18:03:16 +0000

    tests passed

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,60 +1,80 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+
+pd.options.mode.copy_on_write = True
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+bmi = df['weight'] / pow(df['height'] / 100, 2)
+df['overweight'] = 0
+df.loc[bmi > 25, 'overweight'] = 1
 
 # 3
-
+df.loc[df['cholesterol'] <= 1, 'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
+df.loc[df['gluc'] <= 1, 'gluc'] = 0
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+   # 5
+   global df
+   df_cat = df.melt(
+      ['cardio'],
+      ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
+   # 6
+   df_cat = df_cat.groupby(
+      ['cardio', 'variable', 'value'],
+      as_index = False).value_counts()
+   df_cat = df_cat.rename(columns = {'count': 'total'})
+   
+   # 7
+   chart = sns.catplot(
+      df_cat,
+      col = 'cardio',
+      hue = 'value',
+      kind = 'bar',
+      x = 'variable',
+      y = 'total')
 
-    # 6
-    df_cat = None
-    
+   # 8
+   fig = chart.figure
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
-    fig.savefig('catplot.png')
-    return fig
-
+   # 9
+   fig.savefig('catplot.png')
+   return fig
 
 # 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+   # 11
+   global df
+   df_heat = df[(df['ap_lo'] < df['ap_hi']) &
+      (df['height'] >= df['height'].quantile(0.025)) &
+      (df['height'] <= df['height'].quantile(0.975)) &
+      (df['weight'] >= df['weight'].quantile(0.025)) &
+      (df['weight'] <= df['weight'].quantile(0.975))]
 
-    # 12
-    corr = None
+   # 12
+   corr = df_heat.corr()
 
-    # 13
-    mask = None
+   # 13
+   mask = np.triu(np.ones(corr.shape))
 
+   # 14
+   fig, ax = plt.subplots()
 
+   # 15
+   map = sns.heatmap(
+      corr,
+      annot = True,
+      fmt = '.1f',
+      mask = mask)
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
-    fig.savefig('heatmap.png')
-    return fig
+   # 16
+   fig.savefig('heatmap.png')
+   return fig
